115d22a3f6d92669d0ce84670546f64123ebfc46
Augelvin Theone Dermaga <augelvintd@gmail.com>
2022-12-26 13:07:22 -0800

    add cells to every row div

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -11,3 +11,13 @@ for (i = 0; i < grid; i++) {
     row.setAttribute('id', `${i+1}`);
 }
 
+const rows = document.querySelectorAll('.rows');
+
+// make cells according to grid size on each row
+rows.forEach((row) => {
+    for (j = 0; j < grid; j++) {
+        const cell = document.createElement('div');
+        row.appendChild(cell);
+        cell.setAttribute('id', `${j+1}`);
+    }
+});
